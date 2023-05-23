761ec3de70b96ad1f5ca503a7d364cbe3abde0d1
antar-cd <sagar.chandradas1994@gmail.com>
2023-05-24 00:01:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+
+app.use(cors());
+app.use(express.json());
+
+const users = [
+    {id: 1, name: 'antar', email: 'antu@gmail.com'},
+    {id: 2, name: 'hridoy', email: 'hridoy@gmail.com'},
+    {id: 3, name: 'bristy', email: 'bristy@gmail.com'}
+]
+
+app.get('/', (req,res) => {
+    res.send ('users Mangement server is running')
+
+})
+
+
+app.get('/users', (req,res) =>{
+    res.send(users);
+})
+
+app.post('/users', (req,res) =>{
+    console.log('post api hitting')
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+})
+
+app.listen(port, () =>{
+    console.log(`server is running on PORT: ${port}`)
+})
